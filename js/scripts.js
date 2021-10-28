ece4e3878a66a952ceea218218a928bec5a607b0
Nathan Conn <nconn34@gmail.com>
2021-10-27 19:00:45 -0700

    Added business logic functions to js file

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    return number1/number2;
+}
+
+const number1 = parseInt(prompt("Enter the first number"));
+const number2 = parseInt(prompt("Enter the second number"));
+
+const resultAdd = add(number1, number2);
+const resultSub = subtract(number1,number2);
+const resultMult = multiply(number1,number2);
+const resultDiv = divide(number1, number2);
+
+
+alert("addition: " + resultAdd + ", " + "subtraction: " + resultSub + ", " +
+"multiplication: " + resultMult + ", " + "division: " + resultDiv + ".");
